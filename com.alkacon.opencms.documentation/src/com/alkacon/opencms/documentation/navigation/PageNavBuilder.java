0d1a8625e457f7534468b84fb30cfd8fffd39872
dSeidel <d.seidel@alkacon.com>
2014-07-31 13:56:30 +0200

    Adjusted NavigationBuilder to flat section structure.

--- com.alkacon.opencms.documentation/src/com/alkacon/opencms/documentation/navigation/PageNavBuilder.java
+++ com.alkacon.opencms.documentation/src/com/alkacon/opencms/documentation/navigation/PageNavBuilder.java
@@ -1,21 +1,21 @@
 
 package com.alkacon.opencms.documentation.navigation;
 
-import org.opencms.configuration.CmsConfigurationException;
+import com.alkacon.opencms.documentation.sectionindexer.SectionIndexer;
+
 import org.opencms.file.CmsObject;
 import org.opencms.file.CmsResource;
 import org.opencms.file.types.I_CmsResourceType;
 import org.opencms.jsp.util.CmsJspContentAccessBean;
+import org.opencms.jsp.util.CmsJspStandardContextBean;
 import org.opencms.loader.CmsLoaderException;
 import org.opencms.loader.CmsResourceManager;
 import org.opencms.main.CmsException;
 import org.opencms.main.OpenCms;
-import org.opencms.security.CmsRoleViolationException;
 import org.opencms.xml.containerpage.CmsContainerBean;
 import org.opencms.xml.containerpage.CmsContainerElementBean;
 import org.opencms.xml.containerpage.CmsContainerPageBean;
 
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -45,60 +45,118 @@ import java.util.List;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
+/**
+ * Builder for a page-navigation in the documentation. 
+ * Builds up a list of navigation elements similar to the OpenCms navigation mechanism.
+ */
 public class PageNavBuilder {
 
+    /** Type name of the sections that should appear in the navigation. */
     private static String SECTION_TYPE_NAME = "documentation-section";
 
-    private CmsContainerPageBean m_page;
+    /** Container where to search for sections. */
     private CmsContainerBean m_container;
+
+    /** CmsObject for the request context in which the page for which the navigation is build is accessed. */
     private CmsObject m_cmsObject;
+
+    /** Type-id of the sections that should appear in the navigation. Initialized lazily. */
     private Integer m_sectionTypeId = null;
 
-    public PageNavBuilder(CmsContainerPageBean page, CmsContainerBean container, CmsObject cmsObject) {
+    /** Current position in the navigation */
+    private int m_position;
 
-        m_page = page;
-        m_container = container;
-        m_cmsObject = cmsObject;
+    /** Section indexer to generate the section numbers */
+    private SectionIndexer m_indexer;
+
+    /** List of the navigation elements extracted from the sections. */
+    private List<PageNavElement> m_navElements;
+
+    /**
+     * @param cmsBean the current CmsJspStandardContextBean
+     * @param containerName name of the container where the content that should be navigated is in.
+     */
+    public PageNavBuilder(final CmsJspStandardContextBean cmsBean, final String containerName) {
+
+        CmsContainerPageBean containerpage = cmsBean.getPage();
+        m_container = containerpage.getContainers().get(containerName);
+        m_cmsObject = cmsBean.getVfs().getCmsObject();
+        m_position = 1;
+        m_navElements = new LinkedList<PageNavElement>();
     }
 
-    public List<PageNavElement> getElements() {
+    /**
+     * @return list of navigation elements
+     */
+    public List<PageNavElement> getNavElements() {
 
-        if (m_container == null) {
-            return null;
+        // collect elements if possible and not already done
+        if ((m_container != null) && m_navElements.isEmpty()) {
+            collectNavElements();
         }
+        return m_navElements;
+    }
 
-        List<PageNavElement> elements = null;
+    /**
+     * Collect navigation elements with error handling.
+     */
+    private void collectNavElements() {
+
         try {
-            elements = getElements(m_container, 1, "");
-        } catch (CmsException e) {
+            collectNavElementsInternal();
+        } catch (Exception e) {
             System.err.println(e.getStackTrace());
         }
-        return elements;
     }
 
-    private List<PageNavElement> getElements(CmsContainerBean container, int level, String prefix) throws CmsException {
+    /**
+     * Collect navigation elements.
+     * @throws CmsException 
+     */
+    private void collectNavElementsInternal() throws CmsException {
 
-        List<PageNavElement> elements = new LinkedList<PageNavElement>();
-        int position = 1;
-        for (CmsContainerElementBean elem : container.getElements()) {
-            elem.initResource(m_cmsObject);
-            CmsResource elemResource = elem.getResource();
-            if (elemResource.getTypeId() == getSectionTypeId()) {
-                CmsJspContentAccessBean sectionContent = new CmsJspContentAccessBean(m_cmsObject, elemResource);
-                String newPrefix = prefix.isEmpty() ? Integer.toString(position) : prefix
-                    + "."
-                    + Integer.toString(position);
-                String title = newPrefix + " " + sectionContent.getValue().get("Headline").toString();
-                elements.add(new PageNavElement(level, position, title, newPrefix));
-                elements.addAll(getPossibleSubElements(elem, level, newPrefix));
-                position += 1;
+        m_indexer = new SectionIndexer();
+
+        for (CmsContainerElementBean elem : m_container.getElements()) {
+            CmsResource elemResource = getElemResource(elem);
+            if (isSection(elemResource)) {
+                String levelString = elem.getSettings().get("level");
+                // the following works because the element setting is present only if explicitly set
+                // default values are not present, thus take the default value if the setting is not present. 
+                int level = levelString != null ? Integer.valueOf(levelString).intValue() : 1;
+                addSectionToPageNav(elemResource, level);
             }
         }
-        return elements;
     }
 
-    private int getSectionTypeId() throws CmsRoleViolationException, CmsConfigurationException, CmsLoaderException {
+    /**
+     * @param elem container element where the resource should be returned from
+     * @return resource of the container element
+     * @throws CmsException
+     */
+    private CmsResource getElemResource(final CmsContainerElementBean elem) throws CmsException {
+
+        elem.initResource(m_cmsObject);
+        return elem.getResource();
+    }
+
+    /**
+     * Checks if a resource has the section type.
+     * 
+     * @param resource the resource to check
+     * @return flag, indicating if the resource has the section type.
+     * @throws CmsLoaderException
+     */
+    private boolean isSection(final CmsResource resource) throws CmsLoaderException {
+
+        return resource.getTypeId() == getSectionTypeId();
+    }
+
+    /**
+     * @return the section type id.
+     * @throws CmsLoaderException
+     */
+    private int getSectionTypeId() throws CmsLoaderException {
 
         if (m_sectionTypeId == null) {
             CmsResourceManager manager = OpenCms.getResourceManager();
@@ -108,16 +166,17 @@ public class PageNavBuilder {
         return m_sectionTypeId.intValue();
     }
 
-    private List<PageNavElement> getPossibleSubElements(CmsContainerElementBean elem, int level, String prefix)
-    throws CmsException {
+    /**
+     * Adds a section to the list of navigation elements.
+     * @param section the section to add
+     * @param level the level of the section
+     */
+    private void addSectionToPageNav(final CmsResource section, final int level) {
 
-        String elemInstanceId = elem.getInstanceId();
-        Collection<CmsContainerBean> containers = m_page.getContainers().values();
-        for (CmsContainerBean container : containers) {
-            if ((container.getParentInstanceId() != null) && container.getParentInstanceId().equals(elemInstanceId)) {
-                return getElements(container, level + 1, prefix);
-            }
-        }
-        return new LinkedList<PageNavElement>();
+        CmsJspContentAccessBean sectionContent = new CmsJspContentAccessBean(m_cmsObject, section);
+        String index = m_indexer.getSectionIndex(level);
+        String title = index + " " + sectionContent.getValue().get("Headline").toString();
+        m_navElements.add(new PageNavElement(level, m_position, title, index));
+        m_position += 1;
     }
 }
