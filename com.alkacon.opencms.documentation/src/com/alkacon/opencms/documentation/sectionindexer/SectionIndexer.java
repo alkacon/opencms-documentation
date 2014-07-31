cd3788c482437c612b3268e380eed2ec25f8117f
dSeidel <d.seidel@alkacon.com>
2014-07-31 12:28:34 +0200

    Changed section indexer to work for the flat ordering of sections.

--- com.alkacon.opencms.documentation/src/com/alkacon/opencms/documentation/sectionindexer/SectionIndexer.java
+++ com.alkacon.opencms.documentation/src/com/alkacon/opencms/documentation/sectionindexer/SectionIndexer.java
@@ -27,56 +27,100 @@
 
 package com.alkacon.opencms.documentation.sectionindexer;
 
-import java.util.LinkedList;
-import java.util.List;
-
+/**
+ * The section indexer calculates the section numbering for
+ * sections in the documentation.
+ * 
+ * To index a section call {@link #getSectionIndex(int)}. This
+ * returns the section number and adjusts the internal section
+ * counter.
+ * 
+ * The indexer is able to handle at most for levels. 
+ * That is, "2.1.3.2" is ok, but "2.1.3.2.1" not.
+ */
 public class SectionIndexer {
 
-    private List<Integer> m_prefix;
-    private Integer m_sectionCount;
+    /** internal section counter */
+    private int[] m_index;
 
+    /**
+     * Default constructor. Initializes the internal section counter.
+     */
     public SectionIndexer() {
 
-        m_prefix = new LinkedList<Integer>();
-        m_sectionCount = 1;
+        m_index = new int[] {0, 0, 0, 0};
     }
 
-    public String getEnterSubsections() {
+    /**
+     * EL-friendly wrapper for getSectionIndex, having the level parameter in it's name
+     * @return Section index
+     */
+    public String getSectionIndex1() {
 
-        m_prefix.add(m_sectionCount);
-        m_sectionCount = 1;
-        return null;
+        return getSectionIndex(1);
     }
 
-    public String getExitSubsections() {
+    /**
+     * EL-friendly wrapper for getSectionIndex, having the level parameter in it's name
+     * @return Section index
+     */
+    public String getSectionIndex2() {
 
-        int lastIndex = m_prefix.size() - 1;
-        m_sectionCount = m_prefix.get(lastIndex);
-        if (!m_prefix.isEmpty()) {
-            m_prefix.remove(lastIndex);
+        return getSectionIndex(2);
+    }
+
+    /**
+     * EL-friendly wrapper for getSectionIndex, having the level parameter in it's name
+     * @return Section index
+     */
+    public String getSectionIndex3() {
+
+        return getSectionIndex(3);
+    }
+
+    /**
+     * EL-friendly wrapper for getSectionIndex, having the level parameter in it's name
+     * @return Section index
+     */
+    public String getSectionIndex4() {
+
+        return getSectionIndex(4);
+    }
+
+    /**
+     * @param level The section level.
+     * @return The index of the section, e.g. "2.1.3"
+     */
+    private String getSectionIndex(int level) {
+
+        increaseSectionIndex(level);
+        return indexToString(level);
+
+    }
+
+    /**
+     * Increases the section index, according to the level of the
+     * current section.
+     * @param level The level of the current section.
+     */
+    private void increaseSectionIndex(final int level) {
+
+        m_index[level - 1] += 1;
+        for (int i = level; i < m_index.length; i++) {
+            m_index[i] = 0;
         }
-        return null;
     }
 
-    public String getExitSection() {
+    /**
+     * @param level The level of the current section
+     * @return The current section's index, e.g. "2.1.3"
+     */
+    private String indexToString(final int level) {
 
-        m_sectionCount += 1;
-        return null;
-    }
-
-    public String getSectionNumber() {
-
-        String sectionNumber = "";
-        for (Integer i : m_prefix) {
-            sectionNumber += (i.toString() + ".");
+        String result = Integer.toString(m_index[0]);
+        for (int i = 1; i < level; i++) {
+            result += "." + Integer.toString(m_index[i]);
         }
-        sectionNumber += m_sectionCount.toString();
-
-        return sectionNumber;
-    }
-
-    public int getSectionLevel() {
-
-        return m_prefix.size() + 1;
+        return result;
     }
 }
