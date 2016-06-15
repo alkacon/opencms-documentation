3a72c8313c8b2400cac18bdff6793944c123f394
dSeidel <d.seidel@alkacon.com>
2016-06-15 11:34:31 +0200

    Added an example for a context menu item provider.

---
+++ com.alkacon.opencms.documentation.examples.contextmenuitem/README.md
@@ -0,0 +1,49 @@
+# Example for a new context menu item #
+Since OpenCms 10 Explorer is very easily configurable.
+Here we show how to add new context menu items.
+
+## Result ##
+When you install the module and restart OpenCms (to load the Jar shipped with the module),
+the context menu for resources in the Explorer is extended - depending on the resources you
+call the menu on:
+ * For a single folder, the menu has not changed
+ * For multiple resources the entry "Greet" with a deactivated sub-entry "Say hello" appears
+ * For a single non-folder resource, the menu is extended by the entry "Greet" with the active sub-entry "Say hello"
+If you click the new entry, an alert box is displayed, saying hello.
+ 
+## Implementation ##
+Just for convenience, we produced a whole module. The only interesting stuff is in the jar, shipped with the module.
+These are the resources in the `src/` subfolder, in particular the folder contains:
+ * A context menu item provider (`ExampleContextMenuItemProvider`)
+ * A context menu item (`ExampleHelloContextMenuItem`)
+ * Localizations (`Messages` and the `.properties` files)
+ * The registration information for the context menu item provider.
+
+### Writing a context menu item provider ###
+A context menu item provider is a class implementing `I_CmsContextMenuItemProvider`.
+It basically provides one or more context menu items and fixes the item structure, i.e., which item is provided as subitem of another.
+
+See the example implementation for more details.
+
+### Writing a context menu item ###
+Context menu items are classes implementing `I_CmsContextMenuItem`.
+Typically, the have a constructor that takes the id of the parent menu item.
+The interface of a context menu item has several methods that allow to:
+ * use items of the same class at different places (via the parent id)
+ * replace items by others (via a priority)
+ * fix the position of the item relative to other items on the same menu level (via the order)
+ * provide a localized item title (via getTitle(locale))
+ * tell when the item is visible and activated or deactivated - typically this depends on the resource selection and the role of the current user
+ * tell if the item is a leaf - meaning if it has sub-items or not.
+ 
+For non-leaf items the class `CmsSubmenu` eases creating such items (see the example provider). Leaf-items you typically implement yourself. See the `ExampleHelloContextMenuItem` for details.
+
+### Localizations ###
+The OpenCms workplace is multilingual. You can change the workplace locale. So your context menu items should usually support multiple languages as well.
+The example reflects how localizations are usually done in OpenCms. 
+
+### Registering the context menu item provider ###
+To get the whole thing working, you have to register your `org.opencms.ui.contextmenu.I_CmsContextMenuItemProvider` implementation.
+
+To do so, add the file `META-INF/services/org.opencms.ui.contextmenu.I_CmsContextMenuItemProvider` to your jar,
+containing just one line: the fully qualified class name of your `I_CmsContextMenuItemProvider` implementation.
---
+++ com.alkacon.opencms.documentation.examples.contextmenuitem/src/com/alkacon/opencms/documentation/examples/contextmenuitem/ExampleHelloContextMenuItem.java
@@ -0,0 +1,154 @@
+/*
+ * File   : $Source$
+ * Date   : $Date$
+ * Version: $Revision$
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (C) 2002 - 2008 Alkacon Software (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.alkacon.opencms.documentation.examples.contextmenuitem;
+
+import org.opencms.file.CmsObject;
+import org.opencms.file.CmsResource;
+import org.opencms.main.OpenCms;
+import org.opencms.ui.I_CmsDialogContext;
+import org.opencms.ui.contextmenu.I_CmsContextMenuItem;
+import org.opencms.workplace.explorer.menu.CmsMenuItemVisibilityMode;
+
+import java.util.List;
+import java.util.Locale;
+
+import com.vaadin.ui.JavaScript;
+import com.vaadin.ui.UI;
+
+/** An example for a context menu item. It just displays a hello message if clicked. */
+public class ExampleHelloContextMenuItem implements I_CmsContextMenuItem {
+
+    /** The id of the parent context menu item. */
+    private String m_parentId;
+
+    /**
+     * @param parentId the id of the parent context menu item.
+     */
+    public ExampleHelloContextMenuItem(String parentId) {
+        m_parentId = parentId;
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsContextMenuItem#executeAction(org.opencms.ui.I_CmsDialogContext)
+     */
+    @Override
+    public void executeAction(I_CmsDialogContext context) {
+
+        JavaScript.getCurrent().execute(
+            "alert('"
+                + Messages.get().getBundle(UI.getCurrent().getLocale()).key(
+                    Messages.GUI_MESSAGE_HELLO_1,
+                    context.getCms().getRequestContext().getCurrentUser().getFullName())
+                + "')");
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsContextMenuItem#getId()
+     */
+    @Override
+    public String getId() {
+
+        return "goodby";
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsContextMenuItem#getOrder()
+     */
+    @Override
+    public float getOrder() {
+
+        return 20;
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsContextMenuItem#getParentId()
+     */
+    @Override
+    public String getParentId() {
+
+        return m_parentId;
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsContextMenuItem#getPriority()
+     */
+    @Override
+    public int getPriority() {
+
+        return 0;
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsContextMenuItem#getTitle(java.util.Locale)
+     */
+    @Override
+    public String getTitle(Locale locale) {
+
+        return Messages.get().getBundle(locale).key(Messages.GUI_CONTEXTMENU_HELLO_0);
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsContextMenuItem#getVisibility(org.opencms.file.CmsObject, java.util.List)
+     */
+    @Override
+    public CmsMenuItemVisibilityMode getVisibility(CmsObject cms, List<CmsResource> resources) {
+
+        if (resources.size() == 1) {
+            CmsResource resource = resources.get(0);
+            if (OpenCms.getResourceManager().matchResourceType("folder", resource.getTypeId())) {
+                return CmsMenuItemVisibilityMode.VISIBILITY_INVISIBLE;
+            } else {
+                return CmsMenuItemVisibilityMode.VISIBILITY_ACTIVE;
+            }
+        }
+        return CmsMenuItemVisibilityMode.VISIBILITY_INACTIVE;
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsHasMenuItemVisibility#getVisibility(org.opencms.ui.I_CmsDialogContext)
+     */
+    @Override
+    public CmsMenuItemVisibilityMode getVisibility(I_CmsDialogContext context) {
+
+        return getVisibility(context.getCms(), context.getResources());
+    }
+
+    /**
+     * @see org.opencms.ui.contextmenu.I_CmsContextMenuItem#isLeafItem()
+     */
+    @Override
+    public boolean isLeafItem() {
+
+        return true;
+    }
+
+}
