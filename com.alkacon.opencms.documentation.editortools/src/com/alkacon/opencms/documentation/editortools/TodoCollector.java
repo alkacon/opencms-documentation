1404bc3f78adb9c55d5bbd61bf5533d78109591a
dSeidel <d.seidel@alkacon.com>
2019-07-03 09:13:16 +0200

    editortools: made TodoCollector more flexible

--- com.alkacon.opencms.documentation.editortools/src/com/alkacon/opencms/documentation/editortools/TodoCollector.java
+++ com.alkacon.opencms.documentation.editortools/src/com/alkacon/opencms/documentation/editortools/TodoCollector.java
@@ -19,7 +19,7 @@
  *
  * For further information about OpenCms, please see the
  * project website: http://www.opencms.org
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -30,6 +30,7 @@ package com.alkacon.opencms.documentation.editortools;
 import com.alkacon.opencms.documentation.editortools.utils.PageFinder;
 
 import org.opencms.file.CmsResource;
+import org.opencms.file.CmsResourceFilter;
 import org.opencms.jsp.CmsJspBean;
 import org.opencms.main.CmsException;
 import org.opencms.main.CmsLog;
@@ -37,6 +38,7 @@ import org.opencms.main.OpenCms;
 import org.opencms.search.CmsSearchException;
 import org.opencms.search.CmsSearchResource;
 import org.opencms.search.solr.CmsSolrIndex;
+import org.opencms.search.solr.CmsSolrQuery;
 import org.opencms.search.solr.CmsSolrResultList;
 import org.opencms.util.CmsUUID;
 
@@ -58,8 +60,6 @@ public class TodoCollector extends CmsJspBean {
     /** The log object for this class. */
     private static final Log LOG = CmsLog.getLog(CmsUUID.class);
 
-    /** Folder where the content of the documentation is stored in (relative to the site path) */
-    private static final String CONTENT_SEARCH_FOLDER = "/opencms-documentation/.content";
     /** Name of the documentation's topic type */
     private static final String TOPIC_TYPE_NAME = "documentation-topic";
     /** Name of the documentation's section type */
@@ -79,19 +79,8 @@ public class TodoCollector extends CmsJspBean {
     /** Locale for which all TODOs are collected for */
     String m_locale;
 
-    /** Initialize the bean and collect all TODOs.
-     * @see org.opencms.jsp.CmsJspBean#init(javax.servlet.jsp.PageContext, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-     */
-    @Override
-    public void init(final PageContext pageContext, final HttpServletRequest request, final HttpServletResponse response) {
-
-        super.init(pageContext, request, response);
-        try {
-            collectTodos();
-        } catch (CmsSearchException e) {
-            LOG.error("CollectionError:", e);
-        }
-    }
+    /** The folders to search in. */
+    String m_folderRestriction;
 
     /**
      * @return Collection of all PageTodos
@@ -101,86 +90,72 @@ public class TodoCollector extends CmsJspBean {
         return m_todos.values();
     }
 
-    /** Collect the TODOs for all Topics and Sections of the documentation.
-     * 
-     * @throws CmsSearchException
+    /** Initialize the bean and collect all TODOs.
+     * @param pageContext current page context.
+     * @param request current request.
+     * @param response current response.
+     * @param searchSiteFolders the site relative folders to search in. Defaults to the root path (of the site).
+     * @see org.opencms.jsp.CmsJspBean#init(javax.servlet.jsp.PageContext, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
-    private void collectTodos() throws CmsSearchException {
+    @Override
+    public void init(
+        final PageContext pageContext,
+        final HttpServletRequest request,
+        final HttpServletResponse response) {
 
-        m_locale = getCmsObject().getRequestContext().getLocale().getLanguage();
-        m_todos = new HashMap<CmsUUID, PageTodos>();
-        collectTopicTodos();
-        collectSectionTodos();
+        init(pageContext, request, response, new String[] {});
     }
 
-    /** Collect all Topic TODOs
-     * 
-     * @throws CmsSearchException
+    /** Initialize the bean and collect all TODOs.
+     * @param pageContext current page context.
+     * @param request current request.
+     * @param response current response.
+     * @param searchSiteFolders the site relative folders to search in. Defaults to the root path (of the site).
+     * @see org.opencms.jsp.CmsJspBean#init(javax.servlet.jsp.PageContext, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
-    private void collectTopicTodos() throws CmsSearchException {
+    public void init(
+        final PageContext pageContext,
+        final HttpServletRequest request,
+        final HttpServletResponse response,
+        final String... searchSiteFolders) {
 
-        CmsSolrIndex index = OpenCms.getSearchManager().getIndexSolr(SOLR_INDEX_NAME);
-        String squery = buildQuery(TOPIC_TYPE_NAME);
-        CmsSolrResultList results = index.search(getCmsObject(), squery);
-        for (CmsSearchResource result : results) {
-            addTopicTodos(result);
+        super.init(pageContext, request, response);
+        String siteRoot = super.getCmsObject().getRequestContext().getSiteRoot();
+        if (!siteRoot.endsWith("/")) {
+            siteRoot = siteRoot + "/";
         }
-    }
-
-    /**
-     * Build the Solr query for collecting TODOs
-     * @param requestedType requested resource type (by name, e.g. TOPIC_TYPE_NAME or SECTION_TYPE_NAME)
-     * @return Solr query for collecting the TODOs from the contents of the given resource type
-     */
-    private String buildQuery(final String requestedType) {
-
-        return "q=todos_"
-            + m_locale
-            + ":[* TO *]&fq=type:"
-            + requestedType
-            + "&parent-folders=\""
-            + this.getRequestContext().getSiteRoot()
-            + CONTENT_SEARCH_FOLDER
-            + "\"&rows=100000";
-    }
-
-    /** Add the TODOs of a found Topic to the pageTodos
-     * 
-     * @param topic Topic found via the Solr search
-     */
-    private void addTopicTodos(final CmsSearchResource topic) {
-
-        List<CmsResource> pages = PageFinder.getDisplayedOnPages(getCmsObject(), topic.getStructureId());
-        TopicBean topicBean = new TopicBean(topic.getStructureId(), topic.getMultivaluedField(TODOS_SOLR_FIELD));
-        for (CmsResource page : pages) {
-            if (!m_todos.containsKey(page.getStructureId())) {
-                try {
-                    m_todos.put(page.getStructureId(), new PageTodos(page, this.getCmsObject()));
-                } catch (CmsException e) {
-                    LOG.error("Could not create PageTodos.", e);
-                }
+        if ((null == searchSiteFolders) || (searchSiteFolders.length == 0)) {
+            m_folderRestriction = "\"" + siteRoot + "\"";
+        } else {
+            String folder = searchSiteFolders[0];
+            if (folder.startsWith("/")) {
+                folder = folder.substring(1);
             }
-            PageTodos pageTodos = m_todos.get(page.getStructureId());
-            pageTodos.addTopic(topicBean);
+
+            if (searchSiteFolders.length == 1) {
+                m_folderRestriction = "\"" + siteRoot + folder + "\"";
+            } else {
+                m_folderRestriction = "(\"" + siteRoot + folder + "\"";
+                for (int i = 1; i < searchSiteFolders.length; i++) {
+
+                    folder = searchSiteFolders[0];
+                    if (folder.startsWith("/")) {
+                        folder = folder.substring(1);
+                    }
+                    m_folderRestriction = m_folderRestriction + " OR \"" + siteRoot + folder + "\"";
+                }
+                m_folderRestriction = m_folderRestriction + ")";
+            }
         }
-    }
-
-    /** Collect all Section TODOs
-     * 
-     * @throws CmsSearchException
-     */
-    private void collectSectionTodos() throws CmsSearchException {
-
-        CmsSolrIndex index = OpenCms.getSearchManager().getIndexSolr(SOLR_INDEX_NAME);
-        String squery = buildQuery(SECTION_TYPE_NAME);
-        CmsSolrResultList results = index.search(getCmsObject(), squery);
-        for (CmsSearchResource result : results) {
-            addSectionTodos(result);
+        try {
+            collectTodos();
+        } catch (CmsSearchException e) {
+            LOG.error("CollectionError:", e);
         }
     }
 
     /** Add a section with it's TODOs to the correct PageTodo in the map of PageTodos
-     * 
+     *
      * @param section
      */
     private void addSectionTodos(final CmsSearchResource section) {
@@ -202,5 +177,95 @@ public class TodoCollector extends CmsJspBean {
             PageTodos pageTodos = m_todos.get(page.getStructureId());
             pageTodos.addSection(sectionBean);
         }
+    }
+
+    /** Add the TODOs of a found Topic to the pageTodos
+     *
+     * @param topic Topic found via the Solr search
+     */
+    private void addTopicTodos(final CmsSearchResource topic) {
+
+        List<CmsResource> pages = PageFinder.getDisplayedOnPages(getCmsObject(), topic.getStructureId());
+        TopicBean topicBean = new TopicBean(topic.getStructureId(), topic.getMultivaluedField(TODOS_SOLR_FIELD));
+        for (CmsResource page : pages) {
+            if (!m_todos.containsKey(page.getStructureId())) {
+                try {
+                    m_todos.put(page.getStructureId(), new PageTodos(page, this.getCmsObject()));
+                } catch (CmsException e) {
+                    LOG.error("Could not create PageTodos.", e);
+                }
+            }
+            PageTodos pageTodos = m_todos.get(page.getStructureId());
+            pageTodos.addTopic(topicBean);
+        }
+    }
+
+    /**
+     * Build the Solr query for collecting TODOs
+     * @param requestedType requested resource type (by name, e.g. TOPIC_TYPE_NAME or SECTION_TYPE_NAME)
+     * @return Solr query for collecting the TODOs from the contents of the given resource type
+     */
+    private CmsSolrQuery buildQuery(final String requestedType) {
+
+        CmsSolrQuery result = new CmsSolrQuery();
+        result.setQuery(TODOS_SOLR_FIELD + ":[* TO *]");
+        result.addFilterQuery("parent-folders:" + m_folderRestriction);
+        result.addFilterQuery("type:" + requestedType);
+        result.setRows(Integer.valueOf(1000000));
+        return result;
+    }
+
+    /** Collect all Section TODOs
+     *
+     * @throws CmsSearchException
+     */
+    private void collectSectionTodos() throws CmsSearchException {
+
+        CmsSolrResultList results = search(SECTION_TYPE_NAME);
+        for (CmsSearchResource result : results) {
+            addSectionTodos(result);
+        }
+    }
+
+    /** Collect the TODOs for all Topics and Sections of the documentation.
+     *
+     * @throws CmsSearchException
+     */
+    private void collectTodos() throws CmsSearchException {
+
+        m_locale = getCmsObject().getRequestContext().getLocale().getLanguage();
+        m_todos = new HashMap<>();
+        collectTopicTodos();
+        collectSectionTodos();
+    }
+
+    /** Collect all Topic TODOs
+     *
+     * @throws CmsSearchException
+     */
+    private void collectTopicTodos() throws CmsSearchException {
+
+        CmsSolrResultList results = search(TOPIC_TYPE_NAME);
+        for (CmsSearchResource result : results) {
+            addTopicTodos(result);
+        }
+    }
+
+    /**
+     * Performs the actual search.
+     * @param typeName the type of content to search.
+     * @return the results.
+     * @throws CmsSearchException thrown if searching fails.
+     */
+    private CmsSolrResultList search(String typeName) throws CmsSearchException {
+
+        CmsSolrIndex index = OpenCms.getSearchManager().getIndexSolr(SOLR_INDEX_NAME);
+        CmsSolrQuery query = new CmsSolrQuery();
+        query.setQuery(TODOS_SOLR_FIELD + ":[* TO *]");
+        query.addFilterQuery("parent-folders:" + m_folderRestriction);
+        query.addFilterQuery("type:" + typeName);
+        query.setRows(Integer.valueOf(1000000));
+        return index.search(getCmsObject(), query, true, null, true, CmsResourceFilter.DEFAULT, 1000000);
+
     }
 }
